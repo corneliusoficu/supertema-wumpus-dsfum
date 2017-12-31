0dcc841f460ef821af5d0b79933f5b8184ca6813
Corneliu Soficu <corneliu.soficu@gmail.com>
2017-12-31 18:52:46 +0200

    Finished algorithm for maze generation with erros.

--- main.ino
+++ main.ino
@@ -4,14 +4,16 @@ void setup()
 {
     Serial.begin(9600);
     while(!Serial);
+    randomSeed(analogRead(0));
     Serial.println(F("Wumpus starting!"));
 
     //TODO: Add test for good division!
 
     char matrix[(NR_ROWS * NR_COLS) / 8];
     
+
     generate_matrix(matrix);
-    print_matrix(matrix);
+    //print_matrix(matrix);
     generate_maze(matrix);
 
 }
--- matrix.cpp
+++ matrix.cpp
@@ -32,29 +32,32 @@ void generate_matrix(char *matrix)
     }
 }
 
-uint8_t get_value_at(char *matrix, uint8_t x, uint8_t y)
-{
-    //print_matrix(matrix);
-    char *c = (matrix + (y * (NR_COLS / 8)));
-    Serial.println((uint8_t)*c);
-    return 0;
-}
-
 int16_t* find_neighbours(int16_t cell)
 {
     int16_t *neighbours = (int16_t*) malloc(4 * sizeof(int16_t));
-    uint8_t index = 0;
+    uint8_t index = random(0,4);
+    Serial.println(index);
 
     neighbours[    index    ] = ((cell - (2 * NR_COLS)) <= 0)? (-1) : (cell - 2 * NR_COLS);   //NORTH
     neighbours[(index+1) % 4] = (cell % NR_COLS == NR_COLS - 1)? (-1) : (cell + 2);           //EAST
     neighbours[(index+2) % 4] = (cell / NR_COLS == NR_ROWS - 2)? (-1) : (cell + 2 * NR_COLS); //SOUTH
-    neighbours[(index+3) % 4] = (cell % NR_COLS != 1)? (cell - 2) : (-1);                     //WEST
+    neighbours[(index+3) % 4] = (cell % NR_COLS > 1)? (cell - 2) : (-1);                     //WEST
 
     return neighbours;
 
 }
 
-void change_cell_in_bit_array(char *bit_array, uint16_t cell, uint8_t value)
+uint8_t get_value_at_cell(char *arr, uint16_t cell)
+{
+    uint8_t byte_val = cell / 8;
+    uint8_t bit_val  = cell % 8;
+
+    char *c = arr + byte_val;
+    uint8_t bit_value =  (*c & (1 << (7 - bit_val))) != 0;
+    return bit_value;
+}
+
+void set_value_at_cell(char *bit_array, uint16_t cell, uint8_t value)
 {
     uint8_t byte_val = cell / 8;
     uint8_t bit_val  = cell % 8;
@@ -70,6 +73,49 @@ void change_cell_in_bit_array(char *bit_array, uint16_t cell, uint8_t value)
     }
 }
 
+uint8_t checkVisited(char *visited, int16_t *neighbours)
+{
+    int16_t *ptr = neighbours;
+    while(ptr < neighbours + 4)
+    {
+        if((*ptr != -1) && get_value_at_cell(visited, *ptr) == 0)
+        {
+            if(ptr != neighbours)
+            {
+                neighbours[0] = *ptr;
+            }
+            return 1;
+        }
+        ++ptr;
+    }
+    return 0;
+}
+
+void clear_wall(char *matrix, uint16_t current_cell, int16_t destination_cell)
+{
+    int8_t direction = destination_cell - current_cell;
+    uint16_t wall_cell;
+
+    if(direction + (2 * NR_COLS) == 0)      //NORTH
+    {
+        wall_cell = destination_cell + NR_COLS;
+    }
+    else if(direction - (2 * NR_COLS) == 0) //SOUTH
+    {
+        wall_cell = destination_cell - NR_COLS;
+    }
+    else if(direction + 2 == 0)             //WEST
+    {
+        wall_cell = destination_cell + 1;
+    }
+    else if(direction - 2 == 0)             //EAST
+    {
+        wall_cell = destination_cell - 1;
+    }
+
+    set_value_at_cell(matrix, wall_cell, 0);
+}
+
 void generate_maze(char *matrix)
 {
     uint16_t unvisited_cells = (NR_ROWS / 2) * (NR_COLS / 2);
@@ -80,25 +126,36 @@ void generate_maze(char *matrix)
 
     uint16_t *sp = stack;
 
-    uint8_t start_cell = 65;
+    uint8_t start_cell = 64;
     uint16_t current_cell = start_cell;
 
-    change_cell_in_bit_array(visited, start_cell, 1);
-
-    //freeRam();
-
-    // while(unvisited_cells)
-    // {
-            int16_t *neighbours = find_neighbours(current_cell);
-
-    // }
-
-
-    //freeRam();
-
-
-
+    set_value_at_cell(visited, start_cell, 1);
     
+    //freeRam();
+
+    while(unvisited_cells)
+    {
+            int16_t *neighbours = find_neighbours(current_cell);
+            if(checkVisited(visited, neighbours))
+            {
+                clear_wall(matrix, current_cell, neighbours[0]);
+                current_cell = neighbours[0];
+
+                set_value_at_cell(visited, current_cell, 1);
+                *sp = current_cell;
+                ++sp;
+                --unvisited_cells;
+            }
+            else
+            {
+                --sp;
+                current_cell = *sp;
+                *sp = 0x00;
+            }
+
+    }
+
+    print_matrix(matrix);
 
 }
 
@@ -120,7 +177,7 @@ void print_matrix(char *matrix)
                 }
                 else if(bit_value == 0)
                 {
-                    Serial.print("_");
+                    Serial.print(" ");
                 }
             }
         }
